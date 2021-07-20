dcb83c1366faf7353d20f9c0c459c7afd9e29b78
연승은 <seungeun_yeon@tmax.co.kr>
2021-07-20 22:38:09 +0900

    feat: add sample container, component and render this

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
-import CounterContainer from "./containers/CounterContainer";
+import SampleContainer from "./containers/SampleContainer";
+// import CounterContainer from "./containers/CounterContainer";
 
 const App = () => {
   return (
     <div>
-      <CounterContainer />
+      {/* <CounterContainer /> */}
+      <SampleContainer />
     </div>
   );
 };
