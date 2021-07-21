5c77d4718e57d8306bd387ec8589f534af4404d8
seungeun_yeon <seungeun_yeon@tmax.co.kr>
2021-07-21 12:27:07 +0900

    feat: apply redux-saga 2 - install redux-devtools-extension

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from "react";
-import SampleContainer from "./containers/SampleContainer";
-// import CounterContainer from "./containers/CounterContainer";
+import React from 'react';
+// import SampleContainer from "./containers/SampleContainer";
+import CounterContainer from './containers/CounterContainer';
 
 const App = () => {
   return (
     <div>
-      {/* <CounterContainer /> */}
-      <SampleContainer />
+      <CounterContainer />
+      {/* <SampleContainer /> */}
     </div>
   );
 };
